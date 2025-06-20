27ec5a40c9de76abd8cdf7ad33eb1e164dfa8b94
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-20 09:36:55 +0900

    Stream filter mapToInt -> for문으로 변경

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,21 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
-        int result = numbers.stream()
-                .filter((n) -> n % 2 == 0) // 2, 4
-                .mapToInt(Integer::intValue)
-                .sum();
+//        int result = numbers.stream()
+//                .filter((n) -> n % 2 == 0) // 2, 4
+//                .mapToInt(x -> x)
+//                .sum();
 
-        System.out.println(result);
+//        System.out.println(result);
+        int sum = 0;
 
+        for (int n : numbers) {
+            if (n % 2 == 0) {
+                sum += n;
+
+            }
+
+        }
+        System.out.println(sum);
     }
 }
